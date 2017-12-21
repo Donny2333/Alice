01d75dd5391b017f7211ba6f32f349a74fcef42d
Donny <1021001390@qq.com>
2017-12-21 20:58:03 +0800

    fix a bug

--- controller/handleEvent.js
+++ controller/handleEvent.js
@@ -31,7 +31,7 @@ const msg = (req, res) => {
   //接收结束
   req.on('end', () => {
     xml2js.parseString(xml, {explicitArray: false}, function (err, json) {
-      if (!err) {
+      if (err) {
         console.log(json)
         return res.end()
       }
