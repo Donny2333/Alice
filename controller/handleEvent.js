ac233246c93c70a7b2ef763618417ca95ff377b1
Donny <1021001390@qq.com>
2017-12-22 09:15:54 +0800

    merge branch orgin/develop into develop

--- controller/handleEvent.js
+++ controller/handleEvent.js
@@ -31,7 +31,8 @@ const msg = (req, res) => {
   //接收结束
   req.on('end', () => {
     xml2js.parseString(xml, {explicitArray: false}, function (err, json) {
-      if (!err) {
+      if (err) {
+	console.log(err)
         console.log(json)
         return res.end()
       }
